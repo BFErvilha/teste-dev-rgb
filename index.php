c992a3b3e634b7e0d00a66fa75cdaccc80e1d256
Bruno Ervilha <bruno.ervilha@shiftinc.com.br>
2021-01-26 18:47:49 -0300

    First Commit
    Build project and config files

---
+++ index.php
@@ -0,0 +1,27 @@
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>RGB Fast</title>
+
+        <!-- Font Awesome -->
+        <link rel="stylesheet" href="/res/site/css/font-awesome.min.css">
+
+        <!-- Custom CSS -->
+        <link rel="stylesheet" href="assets/css/style.css">
+    </head>
+    <body>
+    <?php
+        // Header Element
+        include 'views/includes/header.php';
+
+        //Main Element
+        include 'views/pages/page.php';
+
+        //Footer Element
+        include 'views/includes/footer.php';
+        ?>
+
+    </body>
+</html>
